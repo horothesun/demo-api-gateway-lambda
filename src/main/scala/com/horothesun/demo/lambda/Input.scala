28355d94d82c81ab883885dfa5be1c44efa22c9e
Nicola <horothesun@users.noreply.github.com>
2022-12-04 12:24:18 +0000

    Lambda Input format fix

--- src/main/scala/com/horothesun/demo/lambda/Input.scala
+++ src/main/scala/com/horothesun/demo/lambda/Input.scala
@@ -17,12 +17,8 @@ object Input {
     ).flatMapN(decodedBody)
 
   def bodyEncodingFromInput(in: Map[String, Object]): Option[BodyEncoding] =
-    in.get("isBase64Encoded")
-      .collect { case b: java.lang.Boolean => b }
-      .map(b =>
-        if (b) BodyEncoding.Base64
-        else BodyEncoding.None
-      )
+    in.get("isBase64Encoded").collect { case b: java.lang.Boolean => b }
+      .map(b => if (b) BodyEncoding.Base64 else BodyEncoding.None)
 
   def bodyFromInput(in: Map[String, Object]): Option[String] =
     in.get("body").collect { case s: String => s }
